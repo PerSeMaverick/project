0d6b6293c60690b2b3379acc3a87d8ce06db05d5
PerSeMaverick <skhu151827@gmail.com>
2022-10-30 20:49:25 +0900

    using Portal, useRef

--- project1/src/components/UI/ErorrModule.js
+++ project1/src/components/UI/ErorrModule.js
@@ -1,26 +1,47 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Card from "./Card";
 import Button from "./Button";
 
 import erorrStyle from "./ErorrModule.module.css";
 
+const Backdrop = (props) => {
+  return <div className={erorrStyle.backdrop} onClick={props.onClick} />;
+};
+
+const Overlay = (props) => {
+  return (
+    <Card className={erorrStyle.modal}>
+      <header className={erorrStyle.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={erorrStyle.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={erorrStyle.actions}>
+        <Button onClick={props.onClick}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErorrModule = (props) => {
   return (
-    <div>
-      <div className={erorrStyle.backdrop} onClick={props.onCloseModal} />
-      <Card className={erorrStyle.modal}>
-        <header className={erorrStyle.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={erorrStyle.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={erorrStyle.actions}>
-          <Button onClick={props.onCloseModal}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onCloseModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Overlay
+          title={props.title}
+          message={props.message}
+          onClick={props.onCloseModal}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
--- project1/src/components/Users/AddUser.js
+++ project1/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import Button from "../UI/Button";
 import ErorrModule from "../UI/ErorrModule";
@@ -6,24 +6,10 @@ import ErorrModule from "../UI/ErorrModule";
 import styles from "./AddUser.module.css";
 
 function AddUser(props) {
-  const [usernameInput, setUsernameInput] = useState("");
-  const [ageInput, setAgeInput] = useState("");
-  const [isValid, setIsValid] = useState(true);
+  const usernameInputRef = useRef();
+  const userAgeInputRef = useRef();
+
   const [erorrModal, setErorrModal] = useState();
-
-  function usernameInputChangeHandler(event) {
-    if (event.target.value) {
-      setIsValid(true);
-      setUsernameInput(event.target.value);
-    }
-  }
-
-  function ageInputChangeHandler(event) {
-    if (event.target.value) {
-      setIsValid(true);
-      setAgeInput(event.target.value);
-    }
-  }
 
   const closeModal = () => {
     setErorrModal(null);
@@ -31,23 +17,25 @@ function AddUser(props) {
 
   function formSubmitHandler(event) {
     event.preventDefault();
-    if (usernameInput.trim().length === 0 || ageInput.trim().length === 0) {
-      setIsValid(!isValid);
+
+    const enteredName = usernameInputRef.current.value;
+    const enteredAge = userAgeInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setErorrModal({
         title: "An error ocurred!",
         message: "Please Enter a vaild name and age (non-empty value)",
       });
-    } else if (+ageInput < 1) {
-      setIsValid(!isValid);
+    } else if (+enteredAge < 1) {
       setErorrModal({
         title: "An error ocurred!",
         message: "Please enter a vaild age.",
       });
-    } else if (usernameInput.trim().length !== 0 || ageInput.trim().length !== 0) {
-      props.onAddUser(usernameInput, ageInput);
+    } else if (enteredName.trim().length !== 0 || enteredAge.trim().length !== 0) {
+      props.onAddUser(enteredName, enteredAge);
     }
-    setUsernameInput("");
-    setAgeInput("");
+    usernameInputRef.current.value = "";
+    userAgeInputRef.current.value = "";
   }
 
   return (
@@ -59,25 +47,12 @@ function AddUser(props) {
           onCloseModal={closeModal}
         />
       )}
-      <form
-        className={`${styles.input} ${!isValid && styles.inValid}`}
-        onSubmit={formSubmitHandler}
-      >
+      <form className={`${styles.input}`} onSubmit={formSubmitHandler}>
         <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={usernameInput}
-          onChange={usernameInputChangeHandler}
-        />
+        <input id="username" type="text" ref={usernameInputRef} />
 
         <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          value={ageInput}
-          onChange={ageInputChangeHandler}
-        />
+        <input id="age" type="number" ref={userAgeInputRef} />
         <Button type="submit">Add User</Button>
       </form>
     </div>
